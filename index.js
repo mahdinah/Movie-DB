b38a4f27d5363a413f2fac0981b04d223e3060b0
mahdi halloum <mahdinah98@gmail.com>
2023-01-04 23:38:03 +0200

    step-5

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 3001;
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8, id:12},
+  { title: 'Avatar', year: 2009, rating: 7.8, id:34 },
+  { title: 'Brazil', year: 1985, rating: 8, id:56 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2, id:78 }
+]
 
 app.get('/', (req, res) => {
   res.send('ok')
@@ -46,6 +52,29 @@ app.get('/hello/:ID', (req, res) => {
           )
         }
     })
+      //create
+      app.get('/movies/add', (req, res) => {
+        res.send('add movie')
+
+      })
+         //read
+   app.get('/movies/get', (req, res) => {
+    res.send(
+    {
+     status:200,
+     data:movies
+    }
+  )
+})
+ //update
+ app.get('/movies/edit', (req, res) => {
+  res.send('edit movie')
+ })
+   //delete
+   app.get('/movies/delete', (req, res) => {
+    res.send('delete movie')
+   })
+  
 app.listen(port,()=>{
   console.log(`Server Listening on port ${port}`)
   });
